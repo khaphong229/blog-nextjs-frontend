5dc30f3f3059af300c00db4861174712933e99f6
khaphong229 <khaphong217@gmail.com>
2025-09-24 08:31:03 +0700

    fix: error database

--- src/app/[locale]/marketing/api/counter/route.ts
+++ src/app/[locale]/marketing/api/counter/route.ts
@@ -1,10 +1,5 @@
-import { sql } from 'drizzle-orm';
-import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 import z from 'zod/v4';
-import { db } from '@/libs/DB';
-import { logger } from '@/libs/Logger';
-import { counterSchema } from '@/models/Schema';
 import { CounterValidation } from '@/validations/CounterValidation';
 
 export const PUT = async (request: Request) => {
@@ -17,20 +12,5 @@ export const PUT = async (request: Request) => {
 
   // `x-e2e-random-id` is used for end-to-end testing to make isolated requests
   // The default value is 0 when there is no `x-e2e-random-id` header
-  const id = Number((await headers()).get('x-e2e-random-id')) ?? 0;
-
-  const count = await db
-    .insert(counterSchema)
-    .values({ id, count: parse.data.increment })
-    .onConflictDoUpdate({
-      target: counterSchema.id,
-      set: { count: sql`${counterSchema.count} + ${parse.data.increment}` },
-    })
-    .returning();
-
-  logger.info('Counter has been incremented');
-
-  return NextResponse.json({
-    count: count[0]?.count,
-  });
+  return NextResponse.json({ message: 'Default response' }, { status: 200 });
 };
